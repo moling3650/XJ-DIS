804e8b84a1a1accb178893499cbb75701e9c9acd
moling <365024424@qq.com>
2017-11-10 09:05:45 +0800

    添加JSON辅助类和API入口

---
+++ XJIF/API.ashx.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using XJIF.Utils;
+
+namespace XJIF
+{
+    /// <summary>
+    /// API 的摘要说明
+    /// </summary>
+    public class API : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "application/json;charset=utf-8";
+            var strJson = new { message = "Hello World!" };
+            context.Response.Write(JSONHelper.ToJson(strJson));
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
